56cc7e7dffb63a19e4e684970dc6dcdae813856f
Sandyyuris <72562886+Sandyyuris@users.noreply.github.com>
2025-11-15 20:44:01 +0700

    Y

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +16,15 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        $this->call([
+            RoleSeeder::class,
+        ]);
 
         User::factory()->create([
             'name' => 'Sandy',
-            'username' => 'sandy123',
-            'email' => 'test@example.com',
+            'username' => 'admin',
+            'password' => Hash::make('123' ),
+            'id_role' => 1,
         ]);
     }
 }
